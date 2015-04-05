1aead401bb7b254ce0ae1793dd6afc1a877e77b6
Jorge <jorgecfelix@gmail.com>
2015-04-05 15:08:51 -0700

    Updated Desc

--- WebProxyServer/src/ProxyServer.java
+++ WebProxyServer/src/ProxyServer.java
@@ -1,5 +1,16 @@
 /**
- * Created by JOrge on 4/5/2015.
+ * Created by Jorge Felix and Grant Swenson on 4/5/2015.
+ *
+ * Program Description:
+ Step 2 -  Developing a primitive web server
+ Write a primitive Java web server that would just get back to a client's
+ (i.e. web browser's) request with a 404 response, i.e. resource not found.
+ Your server, however, needs to be able to capture the client's request in
+ full and output it on the command line.
+ *
  */
 public class ProxyServer {
+
+
 }
+
